adc73f8fd72be39486b1ae0460c653a01f7c4079
Hasnain-Mirrani07 <hmirrani3@gmail.com>
2023-05-22 18:04:54 +0500

    first commit

---
+++ lib/utils/assets.dart
@@ -0,0 +1,8 @@
+const String backIcon = "assets/icons/back.png";
+const String walletImg = "assets/images/Wallet-bro 1.png";
+const String amazonImg = "assets/images/amazon.png";
+const String cashPaymentImg = "assets/images/cashPayment.png";
+const String thankyouImg = "assets/images/thankyou.png";
+const String mastercardIcon = "assets/icons/mastercard.png";
+const String alertIcon = "assets/icons/alert.png";
+const String crossIcon = "assets/icons/cross.png";
